f8bde70c0a1b63a6d97698a656c8c1ea73b8a560
Programmer-Develops <shachipandya29@gmail.com>
2026-05-05 09:41:20 +0530

    Add Theme Context, Used, Create and Consumed

--- react-pract/src/main.jsx
+++ react-pract/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ThemeContext from './Context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ThemeContext.Provider value={{color:'blue'}}>
     <App />
-  </StrictMode>,
+  </ThemeContext.Provider>,
 )
